6fae3d0c0870c69e6099aa4da77671c1ecbdc91b
alexander <afanasyev.alexander@gmail.com>
2019-06-23 14:49:16 +0300

    Add catalogue item test class
    Item can be placed into cart test is the only test it contains

---
+++ test_catalogue_item.py
@@ -0,0 +1,12 @@
+import pytest
+# import time
+
+
+class TestCatalogueItem(object):
+    @pytest.mark.parametrize("item_link", ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
+    def test_item_can_be_placed_into_cart(self, browser, item_link):
+        browser.get(item_link)
+        add_to_cart_button = browser.find_element_by_css_selector("button.btn-primary")
+        # time.sleep(20)
+
+        assert add_to_cart_button.is_enabled() is True, "Add to cart button should be enabled"
