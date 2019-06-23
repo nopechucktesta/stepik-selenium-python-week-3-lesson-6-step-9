02e6d96375c9cd5bfcef6ed39db47216a19e5021
alexander <afanasyev.alexander@gmail.com>
2019-06-23 15:07:07 +0300

    Update add to cart button discovery method (even though the old one was also "unique")

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ class TestCatalogueItem(object):
     @pytest.mark.parametrize("item_link", ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
     def test_item_can_be_placed_into_cart(self, browser, item_link):
         browser.get(item_link)
-        add_to_cart_button = browser.find_element_by_css_selector("button.btn-primary")
+        add_to_cart_button = browser.find_element_by_css_selector("button.btn-add-to-basket")
         # time.sleep(20)
 
         assert add_to_cart_button.is_enabled() is True, "Add to cart button should be enabled"
